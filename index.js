80211ba7622a125a763b7938d4f66cf11884c239
Fewatts <feralveswatts@gmail.com>
2023-08-10 16:16:57 -0300

    Js logic part 1

---
+++ index.js
@@ -0,0 +1,92 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+
+/**
+ * Checks if the input string matches the valid format for the calculator.
+ *
+ * @param {string} input - The input string to be validated.
+ * @returns {boolean} Returns true if the input matches the valid format, otherwise false.
+ */
+const isInputValid = (input) => {
+   const regex = /^(\d+)\s*([+\-*/])\s*(\d+)(?:\s*=\s*)?$/;
+   return regex.test(input);
+};
+
+/**
+ * Performs arithmetic calculations based on given numbers and an operator.
+ *
+ * @param {number} num1 - The first number.
+ * @param {string} operator - The arithmetic operator (+, -, *, /).
+ * @param {number} num2 - The second number.
+ * @returns {number|string} The result of the arithmetic operation, or an error message if invalid.
+ */
+const calculate = (num1, operator, num2) => {
+   switch (operator) {
+      case "+":
+         return num1 + num2;
+      case "-":
+         return num1 - num2;
+      case "/":
+         if (num2 === 0) {
+            return "Division by zero!";
+         }
+         return num1 / num2;
+      case "*":
+         return num1 * num2;
+      default:
+         return "Invalid option, try again.";
+   }
+};
+
+/**
+ * Handles button click events and updates the calculator display.
+ *
+ * @param {string} input - The input received from the clicked button.
+ */
+const handleButtonClick = (input) => {
+   if (display.textContent === "Invalid input format.") {
+      display.textContent = input;
+      input = input === "0" ? "" : input;
+   } else if (
+      display.textContent === "0" ||
+      display.textContent === "Division by zero!"
+   ) {
+      display.textContent = input;
+      input = input === "0" ? "" : input;
+   } else if (input === "C") {
+      display.textContent = "0";
+      input = "";
+   } else if (input === "=") {
+      const result = calculateFromString(display.textContent);
+      display.textContent = result;
+      input = result.toString();
+   } else {
+      display.textContent += input;
+      input += input;
+   }
+};
+
+/**
+ * Calculates a result based on a string input in the valid format.
+ *
+ * @param {string} input - The input string in the valid format: "num1 operator num2".
+ * @returns {number|string} The calculated result or an error message if the input is invalid.
+ */
+const calculateFromString = (input) => {
+   if (isInputValid(input)) {
+      const regex = /^(\d+)\s*([+\-*/])\s*(\d+)(?:\s*=\s*)?$/;
+      const match = input.match(regex);
+      const num1 = parseFloat(match[1]);
+      const operator = match[2];
+      const num2 = parseFloat(match[3]);
+      return calculate(num1, operator, num2);
+   } else {
+      return "Invalid input format.";
+   }
+};
+
+buttons.forEach((button) => {
+   button.addEventListener("click", () => {
+      handleButtonClick(button.textContent);
+   });
+});
